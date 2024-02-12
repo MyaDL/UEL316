d8becacb88140e13bb21605e27911db335fc4282
Maxime Canac <maxime.canac@etu.unilim.fr>
2024-02-12 16:22:30 +0100

    branch-dashboard : fix accesdenied , fix createAt -> publishedDat

---
+++ src/Controller/AccessDeniedController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class AccessDeniedController extends AbstractController
+{
+    #[Route('/access-denied', name: 'access_denied')]
+    public function index(): RedirectResponse
+    {
+        return new RedirectResponse($this->generateUrl('home'));
+    }
+}
